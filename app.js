f7724e377dd6e0dc954caa7b7f1f9f76ce877b8b
kevinnail <fumalicious@gmail.com>
2022-09-21 15:42:49 -0700

    show count

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ async function findCountries(name, continent) {
 
     // > Part D: Assign to state the:
     //      - count (of db records)
-
+    count = response.count;
     displayNotifications();
     if (!error) {
         displayCountries();
@@ -83,6 +83,7 @@ function displayNotifications() {
         // > Part D: Display a message with
         //      - how many items were returned in countries array
         //      - how many total matching countries were in the db
+        notificationDisplay.textContent = `Showing ${countries.length} of ${count} countries.`;
     }
 }
 function displayContinentOptions() {
--- fetch-utils.js
+++ fetch-utils.js
@@ -12,7 +12,7 @@ export async function getCountries(name, continent) {
     //   2. order by country name
     //   3. limit to 100 countries
 
-    let query = client.from('countries').select('*').order('name').limit(100);
+    let query = client.from('countries').select('*', { count: 'exact' }).order('name').limit(100);
     if (name) {
         // > Part C: add query for name
         query = query.ilike('name', `%${name}%`);
